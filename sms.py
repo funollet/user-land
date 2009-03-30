d8529b21961fba5baf934a36002a269eb4bd0146
Jordi Funollet <jordi.f@ati.es>
2009-03-30 02:03:29 +0200

    License added.

--- sms.py
+++ sms.py
@@ -24,8 +24,22 @@ Looks like kaddressbook needs the script to be on the PATH. Otherwise it shows a
 .. _`API authentication form`: https://api.upsidewireless.com/soap/Authentication.asmx?op=GetParameters
 """
 
+# Copyright 2009 Jordi Funollet <jordi.f@ati.es>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #TODO:
-# - License.
 # - Test with an HTML proxy.
 # - Test unicode messages.
 
